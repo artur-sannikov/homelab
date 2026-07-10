4dacc2cff6eeeff9cb7655802f2f30df8e947db4
Artur Sannikov <git-sign@asannikov.com>
2026-07-10 22:52:27 +0300

    feat(opentofu): add infisical provider

--- opentofu/providers.tf
+++ opentofu/providers.tf
@@ -1,3 +1,10 @@
+ephemeral "infisical_secret" "proxmox_config" {
+  name         = "PROXMOX_CREDENTIALS"
+  env_slug     = "prod"
+  workspace_id = var.infisical_workspace_id
+  folder_path  = "/"
+}
+
 terraform {
   required_version = "~> 1.15.0"
   required_providers {
@@ -5,16 +12,30 @@ terraform {
       source  = "bpg/proxmox"
       version = "0.111.1"
     }
+    infisical = {
+      source  = "infisical/infisical"
+      version = "0.19.1"
+    }
   }
 }
 
 provider "proxmox" {
-  endpoint  = var.proxmox_endpoint
-  api_token = var.proxmox_api_token
+  endpoint  = jsondecode(ephemeral.infisical_secret.proxmox_config.value)["url"]
+  api_token = jsondecode(ephemeral.infisical_secret.proxmox_config.value)["api_token"]
   insecure  = false
 
   ssh {
     agent    = true
     username = "root"
+  }
+}
+
+provider "infisical" {
+  host = var.infisical_url
+  auth = {
+    universal = {
+      client_id     = var.infisical_client_id
+      client_secret = var.infisical_client_secret
+    }
   }
 }
--- opentofu/variables.tf
+++ opentofu/variables.tf
@@ -1,15 +1,3 @@
-variable "proxmox_endpoint" {
-  description = "Proxmox URL"
-  type        = string
-  sensitive   = true
-}
-
-variable "proxmox_api_token" {
-  description = "Proxmox API Token for opentofu user"
-  type        = string
-  sensitive   = true
-}
-
 variable "codeberg_runner_mac_address" {
   description = "MAC address of the Codeberg runner"
   type        = string
@@ -23,4 +11,22 @@ variable "tailscale_subnet_router_mac_address" {
 variable "ssh_public_keys" {
   description = "SSH keys to add into the VM via cloud-init"
   type        = list(string)
+}
+
+variable "infisical_client_id" {
+  type      = string
+  sensitive = true
+}
+
+variable "infisical_client_secret" {
+  type      = string
+  sensitive = true
+}
+
+variable "infisical_url" {
+  type = string
+}
+
+variable "infisical_workspace_id" {
+  type = string
 }
